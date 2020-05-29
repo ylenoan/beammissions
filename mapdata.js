5b3a4deb19cbb6b6ad268ec88a74f45fbe584703
martinblomq-surf <58287994+martinblomq-surf@users.noreply.github.com>
2020-05-29 18:29:04 +0200

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,950 @@
+
+var simplemaps_worldmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "on_click",
+    state_description: "",
+    state_color: "#e6e6e6",
+    state_hover_color: "#e6e6e6",
+    state_inactive:"false",
+    state_url: "",
+    border_size: 1,
+    all_states_inactive: "yes",
+    all_states_zoomable: "no",
+    location_description: "",
+    location_color: "",
+    location_opacity: "",
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "",
+    location_type: "",
+    location_image_source: "",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+
+    //Zoom settings
+    zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 1,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: .9,
+    zoom_time: 0.5,
+    navigation_color:"#F48E95",
+    navigation_border_color:"#F48E95",
+    adjacent_opacity:".2",
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 1,
+    popup_shadow: 2,
+    popup_corners: 8,
+    popup_font: "12px/1.5 Poppins, Arial, Helvetica, sans-serif",
+    popup_nocss: "yes",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "Donate to this school",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AF: {
+      name: "Afghanistan"
+    },
+    AO: {
+      name: "Angola"
+    },
+    AL: {
+      name: "Albania"
+    },
+    AE: {
+      name: "United Arab Emirates"
+    },
+    AR: {
+      name: "Argentina"
+    },
+    AM: {
+      name: "Armenia"
+    },
+    AU: {
+      name: "Australia"
+    },
+    AT: {
+      name: "Austria"
+    },
+    AZ: {
+      name: "Azerbaijan"
+    },
+    BI: {
+      name: "Burundi"
+    },
+    BE: {
+      name: "Belgium"
+    },
+    BJ: {
+      name: "Benin"
+    },
+    BF: {
+      name: "Burkina Faso"
+    },
+    BD: {
+      name: "Bangladesh"
+    },
+    BG: {
+      name: "Bulgaria"
+    },
+    BH: {
+      name: "Bahrain"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina"
+    },
+    BY: {
+      name: "Belarus"
+    },
+    BZ: {
+      name: "Belize"
+    },
+    BO: {
+      name: "Bolivia"
+    },
+    BR: {
+      name: "Brazil"
+    },
+    BN: {
+      name: "Brunei Darussalam"
+    },
+    BT: {
+      name: "Bhutan"
+    },
+    BW: {
+      name: "Botswana"
+    },
+    CF: {
+      name: "Central African Republic"
+    },
+    CA: {
+      name: "Canada"
+    },
+    CH: {
+      name: "Switzerland"
+    },
+    CL: {
+      name: "Chile"
+    },
+    CN: {
+      name: "China"
+    },
+    CI: {
+      name: "CÃ´te d'Ivoire"
+    },
+    CM: {
+      name: "Cameroon"
+    },
+    CD: {
+      name: "Democratic Republic of the Congo"
+    },
+    CG: {
+      name: "Republic of Congo"
+    },
+    CO: {
+      name: "Colombia"
+    },
+    CR: {
+      name: "Costa Rica"
+    },
+    CU: {
+      name: "Cuba"
+    },
+    CZ: {
+      name: "Czech Republic"
+    },
+    DE: {
+      name: "Germany"
+    },
+    DJ: {
+      name: "Djibouti"
+    },
+    DK: {
+      name: "Denmark"
+    },
+    DO: {
+      name: "Dominican Republic"
+    },
+    DZ: {
+      name: "Algeria"
+    },
+    EC: {
+      name: "Ecuador"
+    },
+    EG: {
+      name: "Egypt"
+    },
+    ER: {
+      name: "Eritrea"
+    },
+    EE: {
+      name: "Estonia"
+    },
+    ET: {
+      name: "Ethiopia"
+    },
+    FI: {
+      name: "Finland"
+    },
+    FJ: {
+      name: "Fiji"
+    },
+    GA: {
+      name: "Gabon"
+    },
+    GB: {
+      name: "United Kingdom"
+    },
+    GE: {
+      name: "Georgia"
+    },
+    GH: {
+      name: "Ghana"
+    },
+    GN: {
+      name: "Guinea"
+    },
+    GM: {
+      name: "The Gambia"
+    },
+    GW: {
+      name: "Guinea-Bissau"
+    },
+    GQ: {
+      name: "Equatorial Guinea"
+    },
+    GR: {
+      name: "Greece"
+    },
+    GL: {
+      name: "Greenland"
+    },
+    GT: {
+      name: "Guatemala"
+    },
+    GY: {
+      name: "Guyana"
+    },
+    HN: {
+      name: "Honduras"
+    },
+    HR: {
+      name: "Croatia"
+    },
+    HT: {
+      name: "Haiti"
+    },
+    HU: {
+      name: "Hungary"
+    },
+    ID: {
+      name: "Indonesia"
+    },
+    IN: {
+      name: "India"
+    },
+    IE: {
+      name: "Ireland"
+    },
+    IR: {
+      name: "Iran"
+    },
+    IQ: {
+      name: "Iraq"
+    },
+    IS: {
+      name: "Iceland"
+    },
+    IL: {
+      name: "Israel"
+    },
+    IT: {
+      name: "Italy"
+    },
+    JM: {
+      name: "Jamaica"
+    },
+    JO: {
+      name: "Jordan"
+    },
+    JP: {
+      name: "Japan"
+    },
+    KZ: {
+      name: "Kazakhstan"
+    },
+    KE: {
+      name: "Kenya"
+    },
+    KG: {
+      name: "Kyrgyzstan"
+    },
+    KH: {
+      name: "Cambodia"
+    },
+    KR: {
+      name: "Republic of Korea"
+    },
+    XK: {
+      name: "Kosovo"
+    },
+    KW: {
+      name: "Kuwait"
+    },
+    LA: {
+      name: "Lao PDR"
+    },
+    LB: {
+      name: "Lebanon"
+    },
+    LR: {
+      name: "Liberia"
+    },
+    LY: {
+      name: "Libya"
+    },
+    LK: {
+      name: "Sri Lanka"
+    },
+    LS: {
+      name: "Lesotho"
+    },
+    LT: {
+      name: "Lithuania"
+    },
+    LU: {
+      name: "Luxembourg"
+    },
+    LV: {
+      name: "Latvia"
+    },
+    MA: {
+      name: "Morocco"
+    },
+    MD: {
+      name: "Moldova"
+    },
+    MG: {
+      name: "Madagascar"
+    },
+    MX: {
+      name: "Mexico"
+    },
+    MK: {
+      name: "Macedonia"
+    },
+    ML: {
+      name: "Mali"
+    },
+    MM: {
+      name: "Myanmar"
+    },
+    ME: {
+      name: "Montenegro"
+    },
+    MN: {
+      name: "Mongolia"
+    },
+    MZ: {
+      name: "Mozambique"
+    },
+    MR: {
+      name: "Mauritania"
+    },
+    MW: {
+      name: "Malawi"
+    },
+    MY: {
+      name: "Malaysia"
+    },
+    NA: {
+      name: "Namibia"
+    },
+    NE: {
+      name: "Niger"
+    },
+    NG: {
+      name: "Nigeria"
+    },
+    NI: {
+      name: "Nicaragua"
+    },
+    NL: {
+      name: "Netherlands"
+    },
+    NO: {
+      name: "Norway"
+    },
+    NP: {
+      name: "Nepal"
+    },
+    NZ: {
+      name: "New Zealand"
+    },
+    OM: {
+      name: "Oman"
+    },
+    PK: {
+      name: "Pakistan"
+    },
+    PA: {
+      name: "Panama"
+    },
+    PE: {
+      name: "Peru"
+    },
+    PH: {
+      name: "Philippines"
+    },
+    PG: {
+      name: "Papua New Guinea"
+    },
+    PL: {
+      name: "Poland"
+    },
+    KP: {
+      name: "Dem. Rep. Korea"
+    },
+    PT: {
+      name: "Portugal"
+    },
+    PY: {
+      name: "Paraguay"
+    },
+    PS: {
+      name: "Palestine"
+    },
+    QA: {
+      name: "Qatar"
+    },
+    RO: {
+      name: "Romania"
+    },
+    RU: {
+      name: "Russia"
+    },
+    RW: {
+      name: "Rwanda"
+    },
+    EH: {
+      name: "Western Sahara"
+    },
+    SA: {
+      name: "Saudi Arabia"
+    },
+    SD: {
+      name: "Sudan"
+    },
+    SS: {
+      name: "South Sudan"
+    },
+    SN: {
+      name: "Senegal"
+    },
+    SL: {
+      name: "Sierra Leone"
+    },
+    SV: {
+      name: "El Salvador"
+    },
+    RS: {
+      name: "Serbia"
+    },
+    SR: {
+      name: "Suriname"
+    },
+    SK: {
+      name: "Slovakia"
+    },
+    SI: {
+      name: "Slovenia"
+    },
+    SE: {
+      name: "Sweden"
+    },
+    SZ: {
+      name: "Swaziland"
+    },
+    SY: {
+      name: "Syria"
+    },
+    TD: {
+      name: "Chad"
+    },
+    TG: {
+      name: "Togo"
+    },
+    TH: {
+      name: "Thailand"
+    },
+    TJ: {
+      name: "Tajikistan"
+    },
+    TM: {
+      name: "Turkmenistan"
+    },
+    TL: {
+      name: "Timor-Leste"
+    },
+    TN: {
+      name: "Tunisia"
+    },
+    TR: {
+      name: "Turkey"
+    },
+    TW: {
+      name: "Taiwan"
+    },
+    TZ: {
+      name: "Tanzania"
+    },
+    UG: {
+      name: "Uganda"
+    },
+    UA: {
+      name: "Ukraine"
+    },
+    UY: {
+      name: "Uruguay"
+    },
+    US: {
+      name: "United States"
+    },
+    UZ: {
+      name: "Uzbekistan"
+    },
+    VE: {
+      name: "Venezuela"
+    },
+    VN: {
+      name: "Vietnam"
+    },
+    VU: {
+      name: "Vanuatu"
+    },
+    YE: {
+      name: "Yemen"
+    },
+    ZA: {
+      name: "South Africa"
+    },
+    ZM: {
+      name: "Zambia"
+    },
+    ZW: {
+      name: "Zimbabwe"
+    },
+    SO: {
+      name: "Somalia"
+    },
+    GF: {
+      name: "France"
+    },
+    FR: {
+      name: "France"
+    },
+    ES: {
+      name: "Spain"
+    },
+    AW: {
+      name: "Aruba"
+    },
+    AI: {
+      name: "Anguilla"
+    },
+    AD: {
+      name: "Andorra"
+    },
+    AG: {
+      name: "Antigua and Barbuda"
+    },
+    BS: {
+      name: "Bahamas"
+    },
+    BM: {
+      name: "Bermuda"
+    },
+    BB: {
+      name: "Barbados"
+    },
+    KM: {
+      name: "Comoros"
+    },
+    CV: {
+      name: "Cape Verde"
+    },
+    KY: {
+      name: "Cayman Islands"
+    },
+    DM: {
+      name: "Dominica"
+    },
+    FK: {
+      name: "Falkland Islands"
+    },
+    FO: {
+      name: "Faeroe Islands"
+    },
+    GD: {
+      name: "Grenada"
+    },
+    HK: {
+      name: "Hong Kong"
+    },
+    KN: {
+      name: "Saint Kitts and Nevis"
+    },
+    LC: {
+      name: "Saint Lucia"
+    },
+    LI: {
+      name: "Liechtenstein"
+    },
+    MF: {
+      name: "Saint Martin (French)"
+    },
+    MV: {
+      name: "Maldives"
+    },
+    MT: {
+      name: "Malta"
+    },
+    MS: {
+      name: "Montserrat"
+    },
+    MU: {
+      name: "Mauritius"
+    },
+    NC: {
+      name: "New Caledonia"
+    },
+    NR: {
+      name: "Nauru"
+    },
+    PN: {
+      name: "Pitcairn Islands"
+    },
+    PR: {
+      name: "Puerto Rico"
+    },
+    PF: {
+      name: "French Polynesia"
+    },
+    SG: {
+      name: "Singapore"
+    },
+    SB: {
+      name: "Solomon Islands"
+    },
+    ST: {
+      name: "SÃ£o TomÃ© and Principe"
+    },
+    SX: {
+      name: "Saint Martin (Dutch)"
+    },
+    SC: {
+      name: "Seychelles"
+    },
+    TC: {
+      name: "Turks and Caicos Islands"
+    },
+    TO: {
+      name: "Tonga"
+    },
+    TT: {
+      name: "Trinidad and Tobago"
+    },
+    VC: {
+      name: "Saint Vincent and the Grenadines"
+    },
+    VG: {
+      name: "British Virgin Islands"
+    },
+    VI: {
+      name: "United States Virgin Islands"
+    },
+    CY: {
+      name: "Cyprus"
+    },
+    RE: {
+      name: "Reunion (France)"
+    },
+    YT: {
+      name: "Mayotte (France)"
+    },
+    MQ: {
+      name: "Martinique (France)"
+    },
+    GP: {
+      name: "Guadeloupe (France)"
+    },
+    CW: {
+      name: "Curaco (Netherlands)"
+    },
+    IC: {
+      name: "Canary Islands (Spain)"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "12.971599",
+      lng: "77.594566",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Bangalore, India</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>Since 2012 the School of Mission (SOM) in Bangalore has been training young people with a missionary vision to get equipped and go back into serving in their communities.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79bf22456463515c1edf8"
+    },
+    "1": {
+      lat: "-33.616083",
+      lng: "150.782249",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>South Pacific</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79db69bcd1c641936e6d9",
+      description: "<p style='margin-top:0;max-width:340px;'>Serving across the region in Australia, New Zealand, Fiji, and Papua New Guinea, the SPSM has been a sponsored program by the BMF since 2012.</p>"
+    },
+    "2": {
+      lat: "50.447731",
+      lng: "30.542721",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Eastern Europe</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The Eastern European School of Missions is located in Kyiv, Ukraine and reaches a large geographical area.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5e84af15cb44b3644a9264eb"
+    },
+    "3": {
+      lat: "41.798594",
+      lng: "-87.671572",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Midwest</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The Midwest school, formerly the Chicago School of Missions has been training students since 2014 and sends regularly missionaries in Europe</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79aaf14b2283a6e31406b"
+    },
+    "4": {
+      lat: "14.626893",
+      lng: "121.034365",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Asia Pacific</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The Asia Pacific School of Missions is located in Manilla, Philippines and has been established since 2015.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79c7f83adba6fbbaf61a6"
+    },
+    "5": {
+      lat: "33.768775",
+      lng: "-84.419927",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Atlanta</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The Atlanta School of Missions has been established since 2016 and sends regularly missionaries in Europe</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79b28050d8725d87ad161"
+    },
+    "6": {
+      lat: "9.136691",
+      lng: "7.409300",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>West Africa</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The West Africa School of Missions was established in 2016 and has been one of our most successful new schools.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79f7f83adba6fbbafae8e"
+    },
+    "7": {
+      lat: "19.392623",
+      lng: "-99.120964",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Latin America</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>Serving across the region in Latin America, this school is located in Mexico City since 2018.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79f242456463515c2378b"
+    },
+    "8": {
+      lat: "55.732597",
+      lng: "37.621637",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>Eurasia</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>The Eurasia area includes schools in Moscow and Saint Petersburg in Russia.</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5ec79971050d8725d87aa198"
+    },
+    paris: {
+      lat: "42.345519",
+      lng: "-71.414942",
+      name: "<h4 style='font-size:16px;font-weight:bold;margin:0;'>New England, USA</h4>",
+      image_url: "https://res.cloudinary.com/martinsurf/image/upload/v1590591745/beam/mappin_xw5fyl.png",
+      opacity: "1",
+      type: "image",
+      size: "48",
+      scale:"no",
+      description: "<p style='margin-top:0;max-width:340px;'>Support the New England School of Missions in Boston, Massachusetts with missionaries in Europe</p>",
+      url: "https://www.beammissions.org/checkout/donate?donatePageId=5e84a62517a58e78aad2b822"
+    }
+  },
+  labels: {},
+  regions: {
+    "0": {
+      name: "North America",
+      states: [
+        "US",
+        "AT",
+        "BE",
+        "CH",
+        "DE",
+        "IT",
+        "LU",
+        "NL",
+        "PT",
+        "FR",
+        "ES",
+        "CZ"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "1": {
+      name: "Mexico",
+      states: [
+        "MX"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "2": {
+      name: "Eastern Europe",
+      states: [
+        "RO",
+        "AL",
+        "BG",
+        "EE",
+        "GR",
+        "HU",
+        "LV",
+        "LT",
+        "MD",
+        "PL",
+        "SI",
+        "HR",
+        "BA",
+        "ME",
+        "MK",
+        "SK",
+        "TR",
+        "CY",
+        "UA",
+        "RS"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "3": {
+      name: "Eurasia",
+      states: [
+        "RU"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "4": {
+      name: "Southern Asia",
+      states: [
+        "IN",
+        "PK",
+        "NP",
+        "BD",
+        "LK"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "5": {
+      name: "Asia Pacific",
+      states: [
+        "PH",
+        "MM"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "6": {
+      name: "South Pacific",
+      states:[
+        "AU",
+        "NZ",
+        "FJ",
+        "PG"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95"
+    },
+    "7": {
+      name: "West Africa",
+      states:[
+        "NG",
+        "GH",
+        "SL",
+        "GN",
+        "GW",
+        "GM"
+      ],
+      color:"#b2b2b2",
+      hover_color:"#F48E95",
+      zoom_percentage:'.4'
+    }
+  }
+};
