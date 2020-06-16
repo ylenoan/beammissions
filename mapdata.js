3706dc953cad0899078febd9cff10a9fc45b1952
martinblomq-surf <58287994+martinblomq-surf@users.noreply.github.com>
2020-06-16 17:16:08 +0200

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -2,6 +2,7 @@
 var simplemaps_worldmap_mapdata={
   main_settings: {
    //General settings
+    // Edited as a test
     width: "responsive", //'700' or 'responsive'
     background_color: "#FFF",
     background_transparent: "yes",
